5890b40aae8d543a8805818410cd9342e1793955
Daniel Westendorf <daniel@prowestech.com>
2017-09-30 14:24:11 -0600

    Remove un-needed redefinition of puma

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,14 +1,5 @@
 require "test_helper"
 
-require "action_dispatch/system_testing/server"
-class ActionDispatch::SystemTesting::Server
-  def register
-    Capybara.register_server :rails_puma do |app, port, host|
-      Rack::Handler::Puma.run(app, Port: port, Threads: "5:5")
-    end
-  end
-end
-
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
